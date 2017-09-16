87071612927e3cc417bba7bc1c81e191c1198f41
derek-haworth <derekhaw1@gmail.com>
2017-09-15 21:06:54 -0500

    ajax request

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,40 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/hawortDe.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      populateBadges(response.courses.completed)
+    }
+  });
+
+  function populateBadges(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
 
 });
